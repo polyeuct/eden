83ab67685960baf5e1c6a6df39334c49e1f5fdde
shromanleo <sovetroll@gmail.com>
2020-10-09 11:20:44 +0300

    Update Lesson 06.py

--- Stepik/Lesson 06.py
+++ Stepik/Lesson 06.py
@@ -5,7 +5,7 @@ if n < 0 or n > 1000:
     print('Вы ввели неверное значение!')
 elif x10 == 1 and x100 != 11:
     print(n, 'программист')
-elif (x10 == 2 or x10 == 3 or x10 == 4) and (x100 != 12 or x100 != 13 or x100 != 14):
+elif (x10 in (2, 3, 4)) and (x100 not in (12, 13, 14)):
     print(n, 'программиста')
 else:
     print(n, 'программистов')
