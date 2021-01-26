62affb4bad33c6879be2d713fc7fde14879c3e30
shromanleo <72493735+shromanleo@users.noreply.github.com>
2021-01-26 18:19:57 +0300

    Чё то поменял, до успеха ещё далеко

--- Stepik/Lesson 11.py
+++ Stepik/Lesson 11.py
@@ -1,9 +1,6 @@
-lst = []
-while True:
-    X = input().split()
-    if X == ['end']:
-        lst.append(X)
-        break
-    else:
-        lst.append(X)
+lst = [[int(i) for i in input().split()]]
+X = input()
+while X != 'end':
+    lst.append([int(i) for i in X.split()])
+    X = input()
 print(lst)
