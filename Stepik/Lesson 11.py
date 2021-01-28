784af4e77aba7e401f30ff042b34065149a4a24c
shromanleo <72493735+shromanleo@users.noreply.github.com>
2021-01-28 23:18:32 +0300

    Работает! Но нужно допилить

--- Stepik/Lesson 11.py
+++ Stepik/Lesson 11.py
@@ -4,9 +4,13 @@ col = []
 while X != 'end':
     row.append([int(i) for i in X.split()])
     X = input()
-for j in range(len(row)):
-    for i in range(len(row)):
-        col += [row[i][j]]
-print(row)
-print(col)
+for i in range(len(row)):
+    for j in range(len(row[i])):
+        if i == len(row) - 1:
+            i = -1
+        if j == len(row[i]) - 1:
+            j = -1
+        print((row[i-1][j]+row[i+1][j])+(row[i][j-1]+row[i][j+1]), end=' ')
+    print()
+
 
