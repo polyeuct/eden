468100057b56be375898b38a393a14c65173b359
shromanleo <72493735+shromanleo@users.noreply.github.com>
2021-06-11 15:03:51 +0300

    1. Движение по 12 заданию основного курса 2. Почти закончил второе задание в блоке "Множества, Словари" курса RW 3. Удалил ненужный код с курса RW

--- Stepik/Lesson 12.py
+++ Stepik/Lesson 12.py
@@ -4,14 +4,24 @@ mtx = [[0 for i in range(x)] for j in range(x)]
 row = 0
 col = 0
 
-for i in range(1, x * x+1):
-    for right in range(x):
-        mtx[row][col] = i
-        col += 1
+for i in range(1, x + 1):
+    mtx[row][col] = i
+    col += 1
 
-print(mtx)
-#for i in mtx:
-#    print(i)
+row += 1
+
+for i in range(x + 1, x * x + 1):
+    mtx[row][col] = i
+
+# for i in range(1, x * x+1):
+#     for right in range(x):
+#         mtx[row][col] = i
+#         col += 1
+
+# print(mtx)
+
+for i in mtx:
+    print(i)
 
 # for row in range(len(mtx)):
 #    for col in range(len(mtx[row])):
--- Stepik/RW exercise 03.py
+++
@@ -1,6 +0,0 @@
-x = int(input())
-check = 0
-for i in range(2, x // 2 + 1):
-    if x % i == 0:
-        check = check + 1
-print(check <= 0)
